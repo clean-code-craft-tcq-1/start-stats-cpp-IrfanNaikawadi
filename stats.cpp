8a6c377ef7e8f3e841d52df7d9d6960c26a737a0
Naikawadi Irfan Shaphik (RBEI/ECP1) <iin4kor@bosch.com>
2021-01-28 16:05:21 +0530

    Added changes in fucntion

--- stats.cpp
+++ stats.cpp
@@ -1,6 +1,6 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& num) {
+Stats Statistics::ComputeStatistics(const std::vector<___>& a) {
     //Implement statistics here
 	Stats.max = *max_element(a.begin(), a.end());
 	Stats.min = *min_element(a.begin(), a.end());
